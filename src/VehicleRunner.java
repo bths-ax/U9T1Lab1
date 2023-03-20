9a207c3f316d6ac03e81c451e9b78949e8bd4102
ax <114583570+bths-ax@users.noreply.github.com>
2023-03-20 08:33:10 -0400

    day like 18 of asking the doe to unblock ssh

---
+++ src/VehicleRunner.java
@@ -0,0 +1,34 @@
+public class VehicleRunner
+{
+	public static void main(String[] args)
+	{ 
+		// testing all available methods on a Vehicle object
+		Vehicle myVehicle = new Vehicle("Some Vehicle", 6);
+		System.out.println(myVehicle.getName());
+		System.out.println(myVehicle.getWheels());
+		myVehicle.move(10);
+		myVehicle.turn(90);
+		myVehicle.brake(0.25);
+
+		// testing all available methods on a Car object
+		Car myCar = new Car("Honda Civic", 4, "leather");
+		System.out.println(myCar.getName()); // inherited method
+		System.out.println(myCar.getWheels()); // inherited method
+		System.out.println(myCar.getFabric()); // method ONLY available on Car objects
+		myCar.move(30); // inherited method
+		myCar.turn(-90); // inherited method
+		myCar.brake(0.50); // inherited method
+		myCar.honk(); // method ONLY available on Car objects
+		myCar.lockDoors(); // method ONLY available on Car objects
+
+		// ---- WRITE YOUR TEST CODE BELOW! ----
+		Bicycle myBicycle = new Bicycle("bike", 2, 5);
+		System.out.println(myBicycle.getName());
+		System.out.println(myBicycle.getWheels());
+		System.out.println(myBicycle.getGearCount());
+		myBicycle.move(10);
+		myBicycle.turn(90);
+		myBicycle.brake(0.25);
+		myBicycle.ringBell();
+	}
+}
